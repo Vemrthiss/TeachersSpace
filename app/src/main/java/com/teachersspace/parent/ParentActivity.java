171be192dc9b1de454e724333b2f6ce1da458c32
Joel <joeltwh1999@gmail.com>
2022-03-24 20:35:05 +0800

    teacher and student activities setup

--- app/src/main/java/com/teachersspace/parent/ParentActivity.java
+++ app/src/main/java/com/teachersspace/parent/ParentActivity.java
@@ -1,16 +1,49 @@
 package com.teachersspace.parent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.teachersspace.R;
+import com.teachersspace.communications.CallEnabledActivity;
 
-public class ParentActivity extends AppCompatActivity {
+public class ParentActivity extends CallEnabledActivity implements BottomNavigationView.OnItemSelectedListener {
+    private static final String TAG = "TeacherActivity";
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    public int getNavFragmentContainer() {
+        return R.id.nav_host_fragment_parent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationViewParent);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.contactsFragmentMain);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(this, getNavFragmentContainer());
+        int navItemId = item.getItemId();
+        if (navItemId == R.id.action_setting) {
+            navController.navigate(R.id.navigate_settings_action_global);
+            return true;
+        } else if (navItemId == R.id.action_contact) {
+            navController.navigate(R.id.navigate_contacts_action_global);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/teachersspace/teacher/TeacherActivity.java
+++ app/src/main/java/com/teachersspace/teacher/TeacherActivity.java
@@ -13,7 +13,7 @@ import com.teachersspace.R;
 import com.teachersspace.communications.CallEnabledActivity;
 
 public class TeacherActivity extends CallEnabledActivity implements BottomNavigationView.OnItemSelectedListener {
-    private final String TAG = "TeacherActivity";
+    private static final String TAG = "TeacherActivity";
 
     BottomNavigationView bottomNavigationView;
 
@@ -28,26 +28,27 @@ public class TeacherActivity extends CallEnabledActivity implements BottomNaviga
         setContentView(R.layout.activity_teacher);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationViewTeacher);
-
         bottomNavigationView.setOnItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.contactsFragmentMain);
-
     }
 
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         NavController navController = Navigation.findNavController(this, getNavFragmentContainer());
-        switch (item.getItemId()) {
-            case R.id.action_setting:
-                navController.navigate(R.id.navigate_settings_action_global);
-                return true;
-
-            case R.id.action_contact:
-                navController.navigate(R.id.navigate_contacts_action_global);
-                return true;
+        int navItemId = item.getItemId();
+        if (navItemId == R.id.action_setting) {
+            navController.navigate(R.id.navigate_settings_action_global);
+            return true;
+        } else if (navItemId == R.id.action_schedule) {
+            navController.navigate(R.id.navigate_schedule_action_global);
+            return true;
+        } else if (navItemId == R.id.action_contact) {
+            navController.navigate(R.id.navigate_contacts_action_global);
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 //
 }
