fa3ee2d617fb2c8dbfe6e51f409eabee63b30f39
Joel <joeltwh1999@gmail.com>
2022-03-24 20:35:05 +0800

    teacher and student activities setup

--- app/src/main/java/com/teachersspace/student/StudentActivity.java
+++ app/src/main/java/com/teachersspace/student/StudentActivity.java
@@ -1,16 +1,44 @@
 package com.teachersspace.student;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.teachersspace.R;
 
-public class StudentActivity extends AppCompatActivity {
+public class StudentActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
+    private static final String TAG = "StudentActivity";
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationViewStudent);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.contactsFragmentMain);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_student);
+        int navItemId = item.getItemId();
+        if (navItemId == R.id.action_setting) {
+            navController.navigate(R.id.navigate_settings_action_global);
+            return true;
+        } else if (navItemId == R.id.action_contact) {
+            navController.navigate(R.id.navigate_contacts_action_global);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
