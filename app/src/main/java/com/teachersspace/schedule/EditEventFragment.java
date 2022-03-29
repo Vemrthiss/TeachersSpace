450e803a898ab14f6cf08492b05e6f8d71fbe1f8
Kevin Lawrence <36805072+klken26@users.noreply.github.com>
2022-03-29 16:39:02 +0800

    hi

---
+++ app/src/main/java/com/teachersspace/schedule/EditEventFragment.java
@@ -0,0 +1,190 @@
+package com.teachersspace.schedule;
+
+
+import static com.teachersspace.auth.SessionManager.getUserUid;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.teachersspace.R;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EditEventFragment extends Fragment {
+
+    private EditText eventNameET;
+    private TextView eventDateTV, eventTimeTV;
+    private String timeFromArr, dateFromArr;
+    private String timeFromArrString;
+    private LocalTime time;
+    private String eventName = "Free Time Allocated";
+    private Button saveButton;
+    private Button deleteButton;
+    private boolean notBooked = false;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference docRef = db.collection("profschedule").document(String.valueOf(getUserUid()));
+    private String teacherid = getUserUid();
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.event_edit, container, false);
+        return v;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+        Bundle b = new Bundle();
+        b = getArguments();
+        timeFromArr = CalendarUtils.selectedDate + " " + b.getString("t") + ":00";
+        timeFromArrString = CalendarUtils.selectedDate.toString() + " " + b.getString("t") + ":00";
+        DateTimeFormatter timeFmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        initWidgets(view);
+        time = LocalDateTime.parse(timeFromArrString, timeFmt).toLocalTime();
+
+
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setText("Time: " + CalendarUtils.formattedShortTime(time));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void initWidgets(View view) {
+        eventDateTV = view.findViewById(R.id.eventDateTV);
+        eventTimeTV = view.findViewById(R.id.eventTimeTV);
+
+        saveButton = view.findViewById(R.id.saveButton);
+        deleteButton = view.findViewById(R.id.deleteButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveEventAction();
+
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DeleteEventAction();
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void saveEventAction() {
+        boolean eventAlreadyMade = false;
+        for (int i = 0; i < Event.eventsList.size(); i++){
+            if (time.equals(Event.eventsList.get(i).getTime())
+                    && CalendarUtils.selectedDate.equals(Event.eventsList.get(i).getDate())){
+                eventAlreadyMade = true;
+            }
+        }
+        if (eventAlreadyMade){
+            Toast.makeText(getContext(), "Event already made in this slot.", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time, notBooked, teacherid,"");
+
+            //creates hashmap
+            HashMap<String, String> userData = hashMapCreator(eventName, CalendarUtils.selectedDate.toString(),
+                    CalendarUtils.formattedShortTime(time), teacherid, String.valueOf(notBooked), "");
+
+            //add events to their respective lists
+            Event.UserTextList.add(userData);
+            Event.eventsList.add(newEvent);
+
+
+            //updates firestore - change method
+            updateFireStore(Event.UserTextList);
+
+            //goes back to daily calendar
+            getActivity().onBackPressed();
+        }
+
+    }
+
+    public void DeleteEventAction() {
+
+        int index_event = -1;
+        int index_text = -1;
+        String dontHave = "There is no existing event in this timeslot to delete.";
+        for (int i = 0; i < Event.eventsList.size(); i++){
+            if (time.equals(Event.eventsList.get(i).getTime())
+                    && CalendarUtils.selectedDate.equals(Event.eventsList.get(i).getDate())){
+                index_event = i;
+                break;
+            }
+        }
+
+        if (index_event != -1){
+
+            Event.eventsList.remove(index_event);
+            Event.UserTextList.remove(index_text);
+
+            //updates firestore - change method
+            updateFireStore(Event.UserTextList);
+
+            getActivity().onBackPressed();
+        }
+
+        else{
+            Toast.makeText(getContext(), "No event here to delete.", Toast.LENGTH_SHORT).show();
+            deleteButton.setTextColor(Color.LTGRAY);
+        }
+
+    }
+
+
+    public static HashMap<String, String> hashMapCreator(String event, String date, String time, String teacher_id, String booking, String student_id)
+    {
+        HashMap<String, String> temp = new HashMap<>();
+        temp.put("Event", event);
+        temp.put("Date", date);
+        temp.put("Time", time);
+        temp.put("Teacher_id", teacher_id);
+        temp.put("isBooked", booking);
+        temp.put("Student_Name", student_id);
+        return temp;
+    }
+
+    private void updateFireStore(List<Map<String, String>> userList){
+        HashMap<String, List> slotObject = new HashMap<String, List>();
+        slotObject.put("slots", userList);
+        //uploads onto firestore
+        db.collection("profschedule").document(String.valueOf(getUserUid())).set(slotObject)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("", "DocumentSnapshot successfully re-written!");
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/teachersspace/schedule/Event.java
@@ -0,0 +1,112 @@
+package com.teachersspace.schedule;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Event
+{
+
+    //used for storing users' events
+    public static ArrayList<Event> eventsList = new ArrayList<>();
+
+    //tempList will be used in generateEventsInEventsList when needed
+
+    //this is the same as eventsList, but it will hold only strings. This is to store data into FireStore;
+    //Will be generated later again when needed.
+    public static List<Map<String, String>> UserTextList = new ArrayList<>();
+
+    //students will refer to this list for button operations
+    public static ArrayList<Event> totalEventsList = new ArrayList<>();
+
+    //to initialise the arrays one time only
+    public static int initialise_counter = 0;
+
+
+    public static ArrayList<Event> eventsForDate(LocalDate date)
+    {
+        ArrayList<Event> events = new ArrayList<>();
+        for(Event event : eventsList)
+        {
+            if(event.getDate().equals(date))
+                events.add(event);
+        }
+        return events;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static ArrayList<Event> eventsForDateAndTime(LocalDate date, LocalTime time)
+    {
+        ArrayList<Event> events = new ArrayList<>();
+
+        for(Event event : eventsList)
+        {
+            int eventHour = event.time.getHour();
+            int cellHour = time.getHour();
+            if(event.getDate().equals(date) && eventHour == cellHour)
+                events.add(event);
+        }
+        return events;
+    }
+
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+    private boolean isBooked;
+    private String teacher_id;
+    private String student_id;
+
+
+    public Event(String name, LocalDate date, LocalTime time, boolean isBooked, String teacher_id ,String student_id)
+    {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.isBooked = isBooked;
+        this.teacher_id = teacher_id;
+        this.student_id = student_id;
+
+
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public LocalDate getDate()
+    {
+        return date;
+    }
+
+    public void setDate(LocalDate date)
+    {
+        this.date = date;
+    }
+
+    public LocalTime getTime()
+    {
+        return time;
+    }
+
+    public void setTime(LocalTime time)
+    {
+        this.time = time;
+    }
+
+    public boolean getBookingStatus()
+    {
+        return isBooked;
+    }
+}
